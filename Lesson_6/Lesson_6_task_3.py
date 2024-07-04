b379830a295612e0115198d444eed2de7a023713
vitaliypankratov <vitalyamist@gmail.com>
2024-07-04 12:11:39 +0300

    dz6

--- Lesson_6/Lesson_6_task_3.py
+++ Lesson_6/Lesson_6_task_3.py
@@ -2,17 +2,18 @@ from  selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
+from  time import sleep
 
 driver = webdriver.Chrome()
 wait = WebDriverWait(driver, 40, 0.1)
 
 try:
-    # driver.maximize_window()
-    driver.get("http://uitestingplayground.com/ajax")
-    blue_button = driver.find_element(By.CSS_SELECTOR, "#ajaxButton").click()
-    text_from_content = wait.until(EC.visibility_of_element_located(
-        (By.CSS_SELECTOR, ".bg-success"))).text
-    print(text_from_content)
+    driver.get(
+        "https://bonigarcia.dev/selenium-webdriver-java/loading-images.html")
+    wait.until(EC.text_to_be_present_in_element((By.ID, "text"), 'Done!'))
+    sleep(2)
+    get_attribute = driver.find_element(By.ID, "award").get_attribute("src")
+    print(get_attribute)
 
 except Exception as ex:
     print(ex)
