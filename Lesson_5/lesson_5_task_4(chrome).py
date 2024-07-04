1129f3c1a4b606ea73944358b10504bb2930109b
vitaliypankratov <vitalyamist@gmail.com>
2024-07-04 10:56:07 +0300

    dz5

---
+++ Lesson_5/lesson_5_task_4(chrome).py
@@ -0,0 +1,26 @@
+import time
+from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
+
+try:
+    driver = webdriver.Chrome()
+
+    # driver.maximize.window()
+    driver.get("http://the-internet.herokuapp.com/entry_ad")
+
+    # ждем пока модальное окно не появится и кнопка "CLOSE" не станет кликабельной
+    wait = WebDriverWait(driver, 10)
+    modal_window = wait.until(EC.visibility_of_element_located((By.CSS_SELECTOR, ".modal")))
+    close_button = wait.until(EC.element_to_be_clickable((By.CSS_SELECTOR, ".modal-footer .close-button")))
+
+    time.sleep(3)
+    # нажимаем кнопку "CLOSE" в модальном окне
+    close_button.click()
+    time.sleep(2)
+
+except Exception as ex:
+    print(ex)
+finally:
+    driver.quit()
